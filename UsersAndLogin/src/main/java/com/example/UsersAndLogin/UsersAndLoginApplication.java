1c8314157b9f5fe06fc7ff00cbacab91088fc891
santos3452 <santos.virga9@gmail.com>
2025-04-09 18:50:17 -0300

    MicroLoginAndUsers

---
+++ UsersAndLogin/src/main/java/com/example/UsersAndLogin/UsersAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.UsersAndLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAndLoginApplication.class, args);
+	}
+
+}
---
+++ UsersAndLogin/src/test/java/com/example/UsersAndLogin/UsersAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UsersAndLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
